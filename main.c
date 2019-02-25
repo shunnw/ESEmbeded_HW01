6152101e2eeaf95f9154881a5dc37e945e1b74f8
shunnw <N96064094@mail.ncku.edu.tw>
2019-02-25 14:29:40 +0800

    modify main.c

--- main.c
+++ main.c
@@ -5,19 +5,20 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+
+int (*p[4])(int, int) = {plus, minus, multiply, divided};
+int n[255];
+
 int main()
 {
+	
 	int a, c;
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	n['+'] = 0;
+    n['-'] = 1;
+    n['*'] = 2;
+    n['/'] = 3;
+	printf("%d %c %d = %d", a, b, c, p[n[b]](a, c));	
 }
